dfa96418df1d7c5a712f4b1c89e47f115380ef0f
Martanto <martanto@live.com>
2026-05-16 21:16:49 +0200

    Update logging.py

--- src/multigas/logging.py
+++ src/multigas/logging.py
@@ -26,6 +26,7 @@ import os
 import sys
 from pathlib import Path
 
+from dotenv import load_dotenv
 from loguru import logger
 
 
@@ -41,6 +42,8 @@ _CONSOLE_FORMAT = (
     "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | "
     "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
 )
+
+load_dotenv(override=True)
 
 logger.remove()
 
